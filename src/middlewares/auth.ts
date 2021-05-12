33fdfd6b9218945aeb58ec12f24302859d920144
nogw <gabrielnogueiraoliveira@gmail.com>
2021-05-11 21:26:22 -0300

    pas.

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken' 
+
+const auth = ( req: Request, res: Response, next: NextFunction ) => {
+
+}
+
+export default auth
