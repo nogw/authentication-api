dd5c69918d4bd7eab7c3f329d6de82607c56f087
nogw <gabrielnogueiraoliveira@gmail.com>
2021-05-11 21:57:24 -0300

    pas.

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,7 +2,21 @@ import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken' 
 
 const auth = ( req: Request, res: Response, next: NextFunction ) => {
-
+  try {
+    const token = req.body.authorization
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET)
+    const userId = decoded.id
+    
+    if ( req.body.authorization != userId ) {
+      throw 'Invalid user ID'
+    } else {
+      next()
+    }
+  } catch (error) {
+    res.status(400).json({
+      error: error
+    })
+  }
 }
 
 export default auth
